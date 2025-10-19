fe346de5d9967e448d2fa96a3928684c8964acef
muhammed sule dabban <muhammedsulai4@gmail.com>
2025-10-19 18:58:29 +0100

    first commit

--- src/test/java/StoreApp/ItemTest.java
+++ src/test/java/StoreApp/ItemTest.java
@@ -3,8 +3,6 @@ package StoreApp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ItemTest {
     Item item = new Item();
 
--- src/test/java/StoreApp/StoreAppTest.java
+++ src/test/java/StoreApp/StoreAppTest.java
@@ -2,8 +2,6 @@ package StoreApp;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StoreAppTest {
     StoreApp storeApp = new StoreApp();
 
